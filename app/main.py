c0963550aa8a4b45fc50af4b44b99c502fce4f84
Rabbir Remote <rabbirbot00@gmail.com>
2023-07-26 07:40:52 +0800

    feat: 处理远程开发所需配置

--- app/main.py
+++ app/main.py
@@ -25,4 +25,4 @@ async def read_items(token: str = Depends(oauth2_scheme)):
 # 执行
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app='main:app', host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app='main:app', host="0.0.0.0", port=8000, reload=True)
