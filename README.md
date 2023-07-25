f4a7739369c3d1e52741155eb495fa76716dd7da
xujinpeng <admin@cs.cheap>
2023-07-26 07:08:09 +0800

    init: 首次提交

---
+++ README.md
@@ -0,0 +1,2 @@
+# config-center-api
+配置中心后端接口
---
+++ app/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+#
+# @AUTHOR: Rabbir
+# @FILE: ~/Projects/sbeer/config-center/app/main.py
+# @DATE: 2023/07/25 周二
+# @TIME: 21:17:03
+#
+# @DESCRIPTION: todo...
+
+
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
+
+app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@app.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
+
+
+# 执行
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app='main:app', host="127.0.0.1", port=8000, reload=True)
